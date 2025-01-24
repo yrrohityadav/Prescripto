3a4db517c097d4b5e6401b68cb7bb83c5a61721b
Your Name <your_email@example.com>
2025-01-24 17:02:01 +0530

    frontend done

---
+++ backend/config/mongodb.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+const connetDB =async()=>{
+    await mongoose.connect()
+}
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+//app config
+const app=express();
+const port =process.env.PORT || 4000
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+
+app.get('/',(req,res)=>{
+    res.send("API WORKING")
+})
+
+app.listen(port,()=>console.log("Server started",port))
